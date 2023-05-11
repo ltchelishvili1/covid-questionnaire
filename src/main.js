763b71b48cf5af91cc3491a0cdf7839a8f32f901
ltchelishvili1 <levan.tchelishvili@redberry.ge>
2023-05-11 18:02:46 +0400

    refactor: relocate vee-validate folder

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import { createApp } from "vue";
 import App from "@/App.vue";
 import router from "@/router";
 import store from "@/store/index";
-import "@/vee-validate/rules";
+import "@/config/vee-validate/rules";
 
 const app = createApp(App);
 
